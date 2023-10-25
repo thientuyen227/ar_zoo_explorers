d254b0d88c5e6a2ee953594c3f188ac577ccbdac
Thiện Tuyến <nguyenthientuyen22702@gmail.com>
2023-10-25 16:59:13 +0700

    -update unity

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,12 +3,9 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  cloud_firestore
   file_selector_windows
-  firebase_auth
   firebase_core
-  flutter_secure_storage_windows
-  flutter_unity_widget
+  flutter_js
   geolocator_windows
   permission_handler_windows
   share_plus
